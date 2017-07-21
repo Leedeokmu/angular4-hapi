ca21c5682ae5f102422d026ad12e173115accee6
freefly3557 <freefly3557@gmail.com>
2017-07-21 10:28:28 +0900

    making structure -> app main files for the kind #2

--- server/main/users/index.ts
+++ server/main/users/index.ts
@@ -1,8 +1,8 @@
 import * as Hapi from "hapi";
-import * as Route from "./user.route";
-import { IServerConfiguration } from "../../configuration/index";
+import { Route } from "./user.route";
+import { IServerConfiguration } from "../../configuration";
 import { IDatabase } from "../../database";
 
-export function init(server: Hapi.Server, config: IServerConfiguration, database: IDatabase){
-  return Route(server, config, database);
+export function init(server: Hapi.Server, config: IServerConfiguration, database: IDatabase) {
+  Route(server, config, database);
 }
--- server/main/users/user.route.ts
+++ server/main/users/user.route.ts
@@ -1,1 +1,11 @@
-export
+import * as Hapi from "hapi";
+import { IServerConfiguration } from "../../configuration";
+import { IDatabase } from "../../database";
+import { UserController } from "./user.controller";
+import construct = Reflect.construct;
+
+export function Route (server: Hapi, config: IServerConfiguration, database: IDatabase){
+  const userController = new UserController();
+
+  }
+}
