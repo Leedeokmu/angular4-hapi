367fc880e95db7f0aa3ff0bedfd3946d88d7c39f
freefly3557 <freefly3557@gmail.com>
2017-08-09 00:30:52 +0900

    user schema added

--- server/main/users/user.ts
+++ server/main/users/user.ts
@@ -1,1 +1,55 @@
+import * as Mongoose from "mongoose";
+import * as Bcrypt from "bcryptjs";
 
+export interface IUser extends Mongoose.Document{
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+  validatePassword(requestPassword): boolean;
+}
+
+export const UserScheme = new Mongoose.Schema(
+  {
+    email: {type: String, unique: true, required: true},
+    name: {type: String, required: true},
+    password: {type: String, required: true}
+  },
+  {
+    timestamps: true
+  }
+);
+
+function hashPassword(password: string): string {
+  if (!password) {
+    return null;
+  }
+  return  Bcrypt.hashSync(password, Bcrypt.genSaltSync(8));
+}
+
+UserScheme.methods.validatePassword = (requestPassword) => {
+  Bcrypt.compareSync(requestPassword, this.password);
+};
+/*
+* middleware - interceptor
+* */
+UserScheme.pre("save", (next) => {
+  const user = this;
+
+  if (!user.isModified("password")) {
+    return next();
+  }
+  user.password = hashPassword(user.password);
+  return next();
+});
+
+UserScheme.pre("findOneAndUpdate", () => {
+  const password = hashPassword(this.getUpdate().$set.password);
+
+  if ( !password ) {
+    return;
+  }
+  this.findOneAndUpdate({}, {password: password});
+});
+export const UserModel = Mongoose.model<IUser>("User", UserScheme);
